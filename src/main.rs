fa511fb2e86d3f2521ced4eb2522c9620daa4fa1
vodiniz <vitor.oliveira.vod@gmail.com>
2022-06-22 18:53:41 -0300

    First commit

---
+++ src/main.rs
@@ -0,0 +1,17 @@
+fn animelist(){
+    println!("Hello, welcome to anilist where you can keep track of which mangas/anime you read.")
+
+    while true {
+
+    }
+}
+
+
+
+
+
+fn main() {
+    println!("Program Starting");
+
+    anilist();
+}
